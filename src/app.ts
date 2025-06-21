00bc2790ded2b724116bdb827a2ad4cb1a0e6b51
Richard Freitas <wesleyrichard30@gmail.com>
2025-06-21 08:46:12 -0300

    feat: add fastify server, set fastify jwt, fastify cookie and create/validate env schema with zod

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import fastifyCookie from '@fastify/cookie';
+import fastifyJwt from '@fastify/jwt';
+import fastify from 'fastify';
+import { ZodError } from 'zod';
+import { env } from './env';
+
+export const app = fastify();
+
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+});
+
+app.register(fastifyCookie);
+
+app.setErrorHandler((error, _request, reply) => {
+  if (error instanceof ZodError) {
+    return reply.status(400).send({ error: 'Validation error', issues: error.format() });
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error);
+  } else {
+    // TODO: Here I should log to an external monitoring service like DataDog/NewRelic/Sentry
+  }
+
+  return reply.status(500).send({ error: 'Internal server error' });
+});
