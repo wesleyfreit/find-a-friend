feeda301cacdf5b688cb5a9bc940fbf38dbeb673
Richard Freitas <wesleyrichard30@gmail.com>
2025-06-22 15:26:31 -0300

    feat: add prisma schema and fastify jwt types

---
+++ prisma/migrations/20250622182158_add_pet_and_organization_with_requirements/migration.sql
@@ -0,0 +1,85 @@
+-- CreateEnum
+CREATE TYPE "PetAge" AS ENUM ('PUPPY', 'YOUNG', 'ADULT', 'SENIOR');
+
+-- CreateEnum
+CREATE TYPE "PetSize" AS ENUM ('EXTRA_SMALL', 'SMALL', 'MEDIUM', 'LARGE', 'EXTRA_LARGE', 'GIANT');
+
+-- CreateEnum
+CREATE TYPE "PetEnergy" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateEnum
+CREATE TYPE "PetIndependency" AS ENUM ('LOW', 'MEDIUM', 'HIGH');
+
+-- CreateEnum
+CREATE TYPE "PetAmbient" AS ENUM ('INDOOR', 'OUTDOOR', 'BOTH');
+
+-- CreateTable
+CREATE TABLE "organzations" (
+    "id" TEXT NOT NULL,
+    "principal" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "cep" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "latitude" DECIMAL(65,30) NOT NULL,
+    "longitude" DECIMAL(65,30) NOT NULL,
+    "phone" TEXT,
+    "password" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+
+    CONSTRAINT "organzations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "pets" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "about" TEXT,
+    "uf" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "org_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3),
+    "age" "PetAge" NOT NULL,
+    "size" "PetSize" NOT NULL,
+    "energy" "PetEnergy" NOT NULL,
+    "independency" "PetIndependency" NOT NULL,
+    "ambient" "PetAmbient" NOT NULL,
+
+    CONSTRAINT "pets_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "medias" (
+    "id" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "pet_id" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "medias_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "requeriments" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "petId" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "requeriments_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organzations_email_key" ON "organzations"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "organzations_phone_key" ON "organzations"("phone");
+
+-- AddForeignKey
+ALTER TABLE "pets" ADD CONSTRAINT "pets_org_id_fkey" FOREIGN KEY ("org_id") REFERENCES "organzations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "medias" ADD CONSTRAINT "medias_pet_id_fkey" FOREIGN KEY ("pet_id") REFERENCES "pets"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "requeriments" ADD CONSTRAINT "requeriments_petId_fkey" FOREIGN KEY ("petId") REFERENCES "pets"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
